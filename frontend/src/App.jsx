9adc05aa35fe0ff1beec6ff856d17b5aa2ec01d9
Amjad Saeed <amjadsaeedmail@gmail.com>
2023-06-14 06:18:58 +0500

    sdsd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 import Home from "./pages/Home";
